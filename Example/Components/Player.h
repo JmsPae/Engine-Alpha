30dfdb70927d07fb9493db3775ac21f9a6e639c8
James Pae <jameslind01@yahoo.co.uk>
2019-01-10 23:45:52 +0100

    started work on player and input manager

---
+++ Example/Components/Player.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <EngineAlpha/Engine.h>
+
+class Player : public alpha::Component {
+public:
+	Player(alpha::) {
+
+	}
+
+	void Update(float dt) override {
+		if (glfwGetKey(window, GLFW_KEY_E))
+	}
+
+	~Player() {
+
+	}
+
+private:
+
+};
