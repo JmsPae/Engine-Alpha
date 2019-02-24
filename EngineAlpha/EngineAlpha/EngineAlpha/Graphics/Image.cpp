cfd712f147cd52847b9f46274a72585ce11d2801
James Pae <jameslind01@yahoo.co.uk>
2019-02-24 15:20:36 +0100

    Fixed Image

--- EngineAlpha/EngineAlpha/EngineAlpha/Graphics/Image.cpp
+++ EngineAlpha/EngineAlpha/EngineAlpha/Graphics/Image.cpp
@@ -1,22 +1,22 @@
 #include "Image.h"
 
 namespace alpha {
-	Image::Image() : Width(0), Height(0), Components(0), m_imageData(nullptr) {}
+	Image::Image() : Width(0), Height(0), Components(0) {}
 
-	Image::Image(int width, int height, glm::vec4 fillColor) : Width(width), Height(height), Components(4), m_imageData(nullptr) {
-		m_imageData = new unsigned char[width * height * Components];
+	Image::Image(int width, int height, glm::vec4 fillColor) : Width(width), Height(height), Components(4) {
+		m_data = std::vector<unsigned char>(width * height * Components);
 		
 		for (size_t y = 0; y < (size_t)width; y++) {
 			for (size_t x = 0; x < (size_t)height; x++) {
 				for (size_t c = 0; c < 4; c++) {
-					m_imageData[4 * ((y * width) + x) + c] = (char)(fillColor[c] * 255);
+					m_data[4 * ((y * width) + x) + c] = (char)(fillColor[c] * 255);
 				}
 			}
 		}
 	}
 
-	Image::Image(std::string path) : Width(0), Height(0), Components(0), m_imageData(nullptr) {
-		m_imageData = stbi_load(("Resources/Textures/" + path).c_str(), &Width, &Height, &Components, 4);
+	Image::Image(std::string path) : Width(0), Height(0), Components(0) {
+		unsigned char *m_imageData = stbi_load(("Resources/Textures/" + path).c_str(), &Width, &Height, &Components, 4);
 
 		if (!m_imageData) {
 			Width = 0;
@@ -25,14 +25,18 @@ namespace alpha {
 
 			printf("Image %s faled to load!\n", path.c_str());
 		}
+		else {
+			m_data = std::vector<unsigned char>(m_imageData, m_imageData + Width * Height * Components);
+			stbi_image_free(m_imageData);
+		}
 	}
 
 	glm::vec4 Image::GetPixel(int x, int y) {
-		if (m_imageData) {
-			float r = m_imageData[4 * ((y * Width) + x) + 0];
-			float g = m_imageData[4 * ((y * Width) + x) + 1];
-			float b = m_imageData[4 * ((y * Width) + x) + 2];
-			float a = m_imageData[4 * ((y * Width) + x) + 3];
+		if (x < Width && y < Height) {
+			float r = m_data[4 * ((y * Width) + x) + 0];
+			float g = m_data[4 * ((y * Width) + x) + 1];
+			float b = m_data[4 * ((y * Width) + x) + 2];
+			float a = m_data[4 * ((y * Width) + x) + 3];
 
 			return glm::vec4(r / 255, g / 255, b / 255, a / 255);
 		}
@@ -40,7 +44,7 @@ namespace alpha {
 	}
 
 	unsigned char *Image::GetPixelData() {
-		return m_imageData;
+		return &m_data[0];
 	}
 
 	Image::~Image() {
